3b88e5f39ca446e2d27befd2ed22a5e6bf2ba774
pannyandrew <scholarandrewpan@gmail.com>
2019-11-05 20:10:23 -0600

    Updates

---
+++ precache-manifest.8ec87259d4b2c01f6cbf17bbc08bf851.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "25639b90d27a80ed2991e39b6f11b1ee",
+    "url": "/pannyport/index.html"
+  },
+  {
+    "revision": "134f3963d38a3fabbab4",
+    "url": "/pannyport/static/css/main.aceffcd5.chunk.css"
+  },
+  {
+    "revision": "3188c1c3d8cbb3e2d14c",
+    "url": "/pannyport/static/js/2.2c4c39bf.chunk.js"
+  },
+  {
+    "revision": "134f3963d38a3fabbab4",
+    "url": "/pannyport/static/js/main.e9732cbb.chunk.js"
+  },
+  {
+    "revision": "3cd719ebe1608405b5c9",
+    "url": "/pannyport/static/js/runtime-main.96a110d3.js"
+  },
+  {
+    "revision": "0e84d5926187e7a3b785febd3a55bf1d",
+    "url": "/pannyport/static/media/glyph-logo_May2016.0e84d592.png"
+  }
+]);
